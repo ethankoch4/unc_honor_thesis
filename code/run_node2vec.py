fb90e238848e1792eb5d424789058be98303cd98
Ethan Koch <ethankoch4@gmail.com>
2018-02-25 14:50:17 -0500

    submitted node2vec jobs; 3 combos of p/q values

--- code/run_node2vec.py
+++ code/run_node2vec.py
@@ -1,30 +1,35 @@
 def main():
-      import networkx as nx
+    import networkx as nx
+    import json
+    import sys
+    if len(sys.argv) < 3:
+        raise ValueError("Must specify both p and q for node2vec (2nd & 3rd args after filepath")
 
-      from utilities import load_scotus_network
-      from node2vec.node2vec import node2vec
+    from utilities import load_scotus_network
+    from node2vec.node2vec import node2vec
 
-      G, issueAreas = load_scotus_network('../data/scotus_network.graphml')
+    G, issueAreas = load_scotus_network('../data/scotus_network.graphml')
 
-      with open('../data/name_to_ia.json', 'w') as fp:
-          json.dump(issueAreas, fp)
-      del issueAreas
+    with open('../data/name_to_ia.json', 'w') as fp:
+        json.dump(issueAreas, fp)
+    del issueAreas
+    print('P equals: {0}'.format(sys.argv[1]))
+    print('Q equals: {0}'.format(sys.argv[2]))
 
-      n2v = node2vec(G=G,
-                     p=1,
-                     q=1,
-                     walk_length=650,
-                     num_walks=650,
-                     window_size=10,
-                     embedding_size=300,
-                     num_iter=100,
-                     min_count=0,
-                     sg=1,
-                     workers=24)
+    n2v = node2vec(G=G,
+                   p=float(sys.argv[1]),
+                   q=float(sys.argv[1]),
+                   walk_length=650,
+                   num_walks=650,
+                   window_size=10,
+                   embedding_size=300,
+                   num_iter=100,
+                   min_count=0,
+                   sg=1,
+                   workers=24)
 
-      model = n2v.run_node2vec()
-
-      model.save('../data/scotus_n2v.node2vec')
+    model = n2v.run_node2vec()
+    model.save('../data/scotus_n2v_{0}_{1}.node2vec'.format(sys.argv[1],sys.argv[2]))
 
 print('BEGINNING NODE2VEC SCRIPT')
 main()
--- code/run_node2vec.sh
+++ code/run_node2vec.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
-echo "SUBMITTING NODE2VEC JOB"
-sbatch -t 6-12 -o run_node2vec.log -N 1 --job-name="run_node2vec" --mem=32768 --wrap="python3 run_node2vec.py"
+echo "SUBMITTING NODE2VEC JOB WITH ARGS"
+echo "$@"
+p=$1
+q=$2
+COMMAND="python3 run_node2vec.py "
+COMMAND+="$p"
+COMMAND+=" "
+COMMAND+="$q"
+LOG="n2v_run_"
+LOG+="$p"
+LOG+="_"
+LOG+="$q"
+NAME="$LOG"
+LOG+=".log"
+sbatch -t 6-12 -o "$LOG" -N 1 --job-name="$NAME" --mem=32768 --wrap="$COMMAND"
+echo "FINISHED"
